8e5b528f4456806af1ee2312c367a252bc8140cf
Jim Safley <jimsafley@gmail.com>
2019-11-27 23:57:04 -0500

    Complete basic fuctionality

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # AHCB KML to GeoJSON
 
-This utility will convert [KML](https://www.opengeospatial.org/standards/kml/) files
-published by the [Atlas of Historical County Boundaries (AHCB)](https://publications.newberry.org/ahcbp/)
-to [GeoJSON](https://tools.ietf.org/html/rfc7946) files.
+This utility will convert [KML](https://www.opengeospatial.org/standards/kml/) published
+by the [Atlas of Historical County Boundaries (AHCB)](https://publications.newberry.org/ahcbp/)
+to [GeoJSON](https://tools.ietf.org/html/rfc7946).
 
 After downloading a KMZ file from [AHCB's download page](https://publications.newberry.org/ahcbp/downloads/united_states.html)
-you must convert it to an uncompressed KML file.
+you must convert it to an uncompressed KML file:
 
 ```
 $ unzip US_HistCounties_ExAKHI_Gen01_KMZ.zip
 $ mv US_HistCounties_ExAKHI_Gen01.kmz US_HistCounties_ExAKHI_Gen01.zip
 $ unzip US_HistCounties_ExAKHI_Gen01.kmz
 ```
+
+To convert the KML to GeoJSON, run the following command:
+
+```
+$ python3 convert.py <kml_file> <county_date>
+```
+
+The command takes two positional arguments:
+
+1. kml_file: The AHCB KML file (e.g. `US_HistCounties_ExAKHI_Gen01.kml`)
+1. county_date: Get counties that existed at this date (YYYY-MM-DD) (e.g. `1926-01-01`)
+
+This will create the GeoJSON file in the same directory.
--- convert.py
+++ convert.py
@@ -1,25 +1,62 @@
 import xml.etree.ElementTree as ET
 from datetime import datetime
+import argparse
+import json
 
-kml_file = 'US_HistCounties_ExAKHI_Gen05.kml'
+parser = argparse.ArgumentParser(description='Convert KML published by the Atlas of Historical County Boundaries (AHCB) to GeoJSON')
+parser.add_argument('kml_file', help='The AHCB KML file')
+parser.add_argument('county_date', help='Get counties that existed at this date (YYYY-MM-DD)')
+args = parser.parse_args()
+
+county_date = datetime.strptime(args.county_date, '%Y-%m-%d')
 namespaces = {'kml': 'http://www.opengis.net/kml/2.2'}
-county_date = datetime(1926, 1, 1)
+parser = ET.XMLParser(encoding='ISO-8859-1')
+root = ET.parse(args.kml_file, parser=parser).getroot()
+geojson_features = []
 
-root = ET.parse(kml_file, parser=ET.XMLParser(encoding='ISO-8859-1')).getroot()
 for folder in root.findall('./kml:Document/kml:Folder', namespaces):
     folder_name = folder.find('./kml:name', namespaces).text
     if 'Counties' == folder_name:
-        for county_folder in folder.findall('./kml:Folder', namespaces):
-            state_terr_name = county_folder.find('./kml:name', namespaces).text
-            print('Parsing {}'.format(state_terr_name))
-            for placemark in county_folder.findall('./kml:Placemark', namespaces):
-                county_name = placemark.find('./kml:name', namespaces).text
-                county_time_span_begin = placemark.find('./kml:TimeSpan/kml:begin', namespaces).text
-                county_time_span_end = placemark.find('./kml:TimeSpan/kml:end', namespaces).text
-                # Get counties that existed at a certain date.
+        for state_terr_folder in folder.findall('./kml:Folder', namespaces):
+            state_terr_folder_name = state_terr_folder.find('./kml:name', namespaces).text
+            for county_placemark in state_terr_folder.findall('./kml:Placemark', namespaces):
+                county_placemark_name = county_placemark.find('./kml:name', namespaces).text
+                county_time_span_begin = county_placemark.find('./kml:TimeSpan/kml:begin', namespaces).text
+                county_time_span_end = county_placemark.find('./kml:TimeSpan/kml:end', namespaces).text
+                # Only convert counties that existed at the specified date.
                 if datetime.strptime(county_time_span_begin, '%Y-%m-%d') <= county_date <= datetime.strptime(county_time_span_end, '%Y-%m-%d'):
-                    schema_data = placemark.find('./kml:ExtendedData/kml:SchemaData', namespaces)
-                    county_ahcb_id = schema_data.find('./kml:SimpleData[@name="ID"]', namespaces).text;
-                    print('\tParsing {}\t{}\t{}\t{}'.format(county_name, county_ahcb_id, county_time_span_begin, county_time_span_end))
-                    for polygon in placemark.findall('./kml:MultiGeometry/kml:Polygon', namespaces):
-                        print(polygon.find('./kml:outerBoundaryIs/kml:LinearRing/kml:coordinates', namespaces).text)
+                    polygons = county_placemark.findall('./kml:MultiGeometry/kml:Polygon', namespaces)
+                    print('{}\t{}'.format(state_terr_folder_name, county_placemark_name))
+                    geojson_coordinates = []
+                    for polygon in polygons:
+                        kml_coordinates = polygon.find('./kml:outerBoundaryIs/kml:LinearRing/kml:coordinates', namespaces).text.splitlines()
+                        kml_coordinates = [coord.strip() for coord in kml_coordinates] # Trim whitespace
+                        kml_coordinates = [coord for coord in kml_coordinates if coord] # Remove empty strings
+                        kml_coordinates = [[float(latlng) for latlng in coord.split(',')[:2]] for coord in kml_coordinates] # Split and cast to float
+                        geojson_coordinates.append(kml_coordinates if (1 == len(polygons)) else [kml_coordinates])
+                    county_schema_data = county_placemark.find('./kml:ExtendedData/kml:SchemaData', namespaces)
+                    geojson_features.append({
+                        'type': 'Feature',
+                        'properties': {
+                            'id': county_schema_data.find('./kml:SimpleData[@name="ID"]', namespaces).text,
+                            'id_num': county_schema_data.find('./kml:SimpleData[@name="ID_NUM"]', namespaces).text,
+                            'fips': county_schema_data.find('./kml:SimpleData[@name="FIPS"]', namespaces).text,
+                            'name': county_schema_data.find('./kml:SimpleData[@name="NAME"]', namespaces).text,
+                            'full_name': county_schema_data.find('./kml:SimpleData[@name="FULL_NAME"]', namespaces).text,
+                            'cnty_type': county_schema_data.find('./kml:SimpleData[@name="CNTY_TYPE"]', namespaces).text,
+                            'area_sqmi': county_schema_data.find('./kml:SimpleData[@name="AREA_SQMI"]', namespaces).text,
+                            'state_terr': county_schema_data.find('./kml:SimpleData[@name="STATE_TERR"]', namespaces).text,
+                            'start_date': county_time_span_begin,
+                            'end_date': county_time_span_end,
+                        },
+                        'geometry': {
+                            'type': 'Polygon' if (1 == len(geojson_coordinates)) else 'MultiPolygon',
+                            'coordinates': geojson_coordinates
+                        },
+                    })
+geojson = {
+    'type': 'FeatureCollection',
+    'features': geojson_features,
+}
+with open('feature-collection-{}.json'.format(args.county_date), 'w') as outfile:
+    outfile.write(json.dumps(geojson))
